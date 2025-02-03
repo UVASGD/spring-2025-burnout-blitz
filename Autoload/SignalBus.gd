7f265dffed11e9a49be899ca750996998e0b19c9
Saaswath Kumar <saaskum12@gmail.com>
2025-02-03 01:12:23 -0500

    finished basic version of driver item system

--- Autoload/SignalBus.gd
+++ Autoload/SignalBus.gd
@@ -3,3 +3,9 @@ extends Node
 signal driver_item_taken
 
 signal driver_giving_item
+
+signal update_engine_force(modifier:String, amount:float)
+
+signal clear_item
+
+signal driver_play_animation
--- Karts/driver_ui.gd
+++ Karts/driver_ui.gd
@@ -4,8 +4,12 @@ extends Control
 
 func _ready():
 	SignalBus.connect("driver_giving_item", update_item_label)
+	SignalBus.connect("clear_item", clear)
 	
 func update_item_label(item_name:String):
 	$current_item.text = item_name
+
+func clear():
+	$current_item.text = ""
 
 	
