96464b3ee4d3f9e5ce6fbeac5b181c51f819ffa2
Saaswath Kumar <saaskum12@gmail.com>
2025-02-17 20:34:49 -0500

    continued work on checkpoints

--- Source/Players/world_scripts/track_settings.gd
+++ Source/Players/world_scripts/track_settings.gd
@@ -1,9 +1,12 @@
 class_name track_settings extends Node3D
 
-@export var num_laps = 3
+@export var num_laps:int = 3
 @export var driver_items_on_map:Array[String] = []
+var start_point:Node3D
+var num_laps_left:int
 
 func _ready():
+	num_laps_left = num_laps
 	SignalBus.connect("driver_item_taken", give_driver_item)
 	
 func give_driver_item():
@@ -13,3 +16,12 @@ func give_driver_item():
 		var random_item_name:String = driver_items_on_map[random_item_number]		
 		SignalBus.emit_signal("driver_giving_item", random_item_name)
 		print("giving item: " + random_item_name)
+
+func has_passed_all_checkpoints(checkpoints:Array[Node3D]):
+	for checkpoint in checkpoints:
+		
+	
+	
+	
+	
+	
