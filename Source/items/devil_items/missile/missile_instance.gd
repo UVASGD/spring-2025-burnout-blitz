6c4634a7612adbaeee2563537a946b971f1bbc8d
Saaswath Kumar <saaskum12@gmail.com>
2025-03-03 03:15:43 -0500

    breakable columns, new missile, new crosshair

--- Source/items/devil_items/missile/missile_instance.gd
+++ Source/items/devil_items/missile/missile_instance.gd
@@ -2,39 +2,25 @@ extends Area3D
 
 var explosion = preload("res://Source/items/devil_items/explosion.tscn")
 
-@export var speed:int = 50
-@export var rotation_speed:int = 3
-@export var explosion_force_amount:int = 5000
+@export var speed: int = 50
+@export var rotation_speed: int = 3
+@export var explosion_force_amount: int = 5000
 var velocity = Vector3()
-var rot = Vector3()
 
-@onready var target:VehicleBody3D
+
+
+@onready var target: VehicleBody3D  # We can ignore this as it's no longer used
 
 func _ready():
-	call_deferred("_set_target")
+	# No need to set target anymore, missile doesn't track anything
+	velocity = -global_transform.basis.z * speed  # Initial velocity in the direction it's facing
 
-func _set_target():
-	var drivers = get_tree().get_nodes_in_group("driver")
-	if drivers.size() > 0:
-		target = drivers[0]
-	else:
-		print("No nodes found in 'driver' group")
 func _physics_process(delta):
-	if target:
-		var direction = (target.global_transform.origin - global_transform.origin).normalized()
-
-		var forward = -global_transform.basis.z  
-		var rotation_axis = forward.cross(direction).normalized()
-		var angle = acos(forward.dot(direction)) 
-
-		if angle > 0.01: 
-			var rotation_amount = min(angle, rotation_speed * delta) 
-			rotate(rotation_axis, rotation_amount)
-
-		# Move forward
-		global_translate(-global_transform.basis.z * speed * delta)
+	# Move the missile in a straight line
+	global_translate(velocity * delta)
 
 func _on_body_entered(body):
+	# When the missile collides with a body
 	if body.is_in_group("driver") or body.is_in_group("track") or body.is_in_group("breakable"):
 		var collision_point = global_transform.origin
 		if body is PhysicsBody3D:
@@ -50,5 +36,8 @@ func _on_body_entered(body):
 		get_parent().add_child(explosion_instance)
 		explosion_instance.play_explosion_anim()
 		hide()
+		
+		if body.is_in_group("breakable"):
+			body.break_object()
 		await explosion_instance.anim_player.animation_finished
 		queue_free()
