d9adab3e4161fd83d103428d41f30d1ca25b1e90
Saaswath Kumar <saaskum12@gmail.com>
2025-04-05 14:55:27 -0400

    small changes

--- Autoload/SignalBus.gd
+++ Autoload/SignalBus.gd
@@ -1,5 +1,6 @@
 extends Node
 
+# Driver signals
 signal driver_item_taken
 signal driver_giving_item
 signal update_engine_force(modifier:String, amount:float)
@@ -9,11 +10,13 @@ signal driver_play_animation_backwards(anim_name: String)
 signal out_of_bounds
 signal driver_change_invul
 
+# Track signals
 signal update_checkpoint(name:String)
 signal revert_checkpoints
-
 signal update_laps_left(laps_left:String)
+signal end_track
 
+# Devil Signals
 signal devil_item_taken
 signal devil_giving_item
 signal clear_devil_item
