60a6654d4fb761e312729edab24533994de419e9
Saaswath Kumar <saaskum12@gmail.com>
2025-04-07 04:15:19 -0400

    small menu changes

--- Source/Menus/map_select_menu.gd
+++ Source/Menus/map_select_menu.gd
@@ -6,3 +6,8 @@ class_name MapSelectMenu
 func _on_button_pressed() -> void:
 	game_container.spawn_level(1)
 	queue_free()
+
+
+func _on_back_pressed():
+	game_container.spawn_main_menu()
+	queue_free()
