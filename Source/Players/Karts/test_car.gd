7c25a820c4ea40944c479351160bf18fca1df6a7
kedrickfudala <KedrickAFudala@gmail.com>
2025-04-27 14:15:09 -0400

    updated main menu and added music

--- Source/Players/Karts/test_car.gd
+++ Source/Players/Karts/test_car.gd
@@ -4,8 +4,8 @@ extends VehicleBody3D
 @export var MAX_STEER: float = 0.9
 @export var steering_curve: Curve
 
-var STEER_SPEED = 7.0  # Increased for quicker steering
-var STEER_LIMIT = 0.5  # More responsive turning
+var STEER_SPEED = 5.0  # Increased for quicker steering
+var STEER_LIMIT = 0.3  # More responsive turning
 var ENGINE_POWER = 400
 
 @export var default_engine_power: float = 300
@@ -27,8 +27,6 @@ func _ready():
 var previous_speed := linear_velocity.length()
 
 func _physics_process(delta: float):
-	var fwd_mps := (linear_velocity * transform.basis).x
-
 	# Improved Steering Responsiveness
 	var steer_input = Input.get_axis("turn_right", "turn_left")
 	steering = move_toward(steering, steer_input * STEER_LIMIT, STEER_SPEED * delta)
